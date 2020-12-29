2512713258d88a4c66ba434bba0183513984cd68
GitHubInter <2686671526@qq.com>
2020-12-29 23:45:33 +0800

    element-ui插件引用

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
   <div id="app">
-    <el-button @click="show">按钮</el-button>
-    <p>hello world</p>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    
-  },
-  methods: {
-    show(){
-      this.$message.success('element-ui提示')
-    }
+    HelloWorld
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
