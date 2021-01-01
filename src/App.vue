6eec8590ed4cacbb58f713dc3cde78e6f9343adf
githubinter <githubinter@outlook.com>
2021-01-01 19:21:41 +0800

    表单属性设置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="app">
-    <el-form inline :model="data" :rules="rules" ref="form" validate-on-rule-change status-icon>
-      <el-form-item label="审批人" prop="user" :validate-status="status" :error="error">
+    <el-form
+      style="width: 500px"
+      label-width="100px"
+      label-position="top"
+      label-suffix="："
+      :inline="false"
+      :model="data"
+      :rules="rules"
+      ref="form"
+      validate-on-rule-change
+      status-icon
+      disabled
+      size="medium"
+    >
+      <el-form-item
+        label="审批人"
+        prop="user"
+        :validate-status="status"
+        :error="error"
+      >
         <el-input v-model="data.user" placeholder="审批人"></el-input>
       </el-form-item>
       <el-form-item label="活动区域">
-        <el-select v-model="data.region" placeholder="活动区域">
+        <el-select
+          v-model="data.region"
+          placeholder="活动区域"
+          style="width: 100%"
+        >
           <el-option label="区域一" value="shanghai"></el-option>
           <el-option label="区域二" value="beijing"></el-option>
         </el-select>
@@ -13,9 +35,9 @@
       <el-form-item>
         <el-button type="primary" @click="onSubmit">查询</el-button>
         <el-button type="primary" @click="addRule">添加校验规则</el-button>
-        <el-button type="success" @click="showSuccess">成功校验</el-button>
+        <!-- <el-button type="success" @click="showSuccess">成功校验</el-button>
         <el-button type="danger" @click="showError">失败校验</el-button>
-        <el-button type="warning" @click="showValidating">校验中</el-button>
+        <el-button type="warning" @click="showValidating">校验中</el-button> -->
       </el-form-item>
     </el-form>
   </div>
@@ -73,17 +95,17 @@ export default {
       this.rules = Object.assign({}, this.rules, { user: newRule });
     },
     showError() {
-      this.status = 'error'
-      this.error = '用户名输入有误'
+      this.status = "error";
+      this.error = "用户名输入有误";
     },
     showSuccess() {
-      this.status = 'success'
-      this.error = ''
+      this.status = "success";
+      this.error = "";
     },
     showValidating() {
-      this.status = 'validating'
-      this.error = ''
-    }
+      this.status = "validating";
+      this.error = "";
+    },
   },
 };
 </script>
